1c4f92db1ca3e5c231b39d09feb1aae2ad85fbc2
DevAman-sudo <devaman.net@gmail.com>
2020-12-12 10:00:57 +0545

    Sucessfully Fetched All Data From REST API And Served

--- Js/main.js
+++ Js/main.js
@@ -8,16 +8,28 @@ const form = document.getElementById('form');
 const textArea = document.getElementById('text-area');
 const formButton = document.getElementById('form-button');
 
+// Fetching User Geo-Location //
+if ('geolocation' in navigator) {
+    console.log('Avibable');
+    // navigator.geolocation.getCurrentPosition( position => {
+        // console.log(position);
+    // });
+} else {
+    Error('Your Device Does Not Support Geo Location');
+}
+
+
 // Fetching All Data Needed From REST API With Async Function //
 async function apiData() {
     // body...
     const api = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Thecho&appid=7f6480c0040676cf62c7fa294ca443aa');
     // Json Converted Data //
     const jsonData = await api.json();
-    console.log(jsonData);
     
     // Serving Fetched Temp On Page //
     mainTemp.innerText = Math.round(jsonData.main.temp - 273.15) ;
+    degreeF.innerText = Math.round(((jsonData.main.temp - 273.15) * 9/5) + 32) + '°F' ;
+    degreeK.innerText = Math.round(jsonData.main.temp) + '°K' ;
     
     // SetUp Country And Place Name To Serve //
     placeName.innerText = jsonData.name ;
