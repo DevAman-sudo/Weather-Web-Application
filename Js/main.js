11464f9a3a27018f23ce543bb29e0ef8c21b3a01
DevAman-sudo <devaman.net@gmail.com>
2020-12-13 10:06:58 +0545

    Solved Json Data / REST API Bug

--- Js/main.js
+++ Js/main.js
@@ -9,28 +9,35 @@ const form = document.getElementById('form');
 const textArea = document.getElementById('text-area');
 const formButton = document.getElementById('form-button');
 
-// Set Api Location For Open Weather Api //
-// let apiLocation = "New York" ;
-
 // Fetching All Data Needed From REST API With Async Function //
 async function apiData(apiLocation) {
 	// body...
 	let api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${apiLocation}&appid=7f6480c0040676cf62c7fa294ca443aa`);
 	// Json Converted Data //
 	let jsonData = await api.json();
-	console.log(jsonData.wind.speed);
-	
-	// Serving Temp Json Data //
-	mainTemp.innerText = Math.round(jsonData.main.temp - 273.15);
-	degreeF.innerText = Math.round(((jsonData.main.temp - 273.15) * 9 / 5) + 32) + '°F';
-	degreeK.innerText = Math.round(jsonData.main.temp) + '°K';
 
-	// SetUp Country And Place Name To Serve // 
-	placeName.innerText = jsonData.name;
-	countryName.innerText = jsonData.sys.country;
-	weatherCondition.innerText = `${jsonData.wind.speed} W/speed`;
+	if (jsonData.cod === '404') {
+		// Changing textArea Class To error-input //
+		textArea.classList = 'error-input' ;
+		textArea.placeholder = 'City Not Found !!! Try Again.' ;
+		
+	} else if (jsonData.cod !== '404') {
+		// Changing Text Area To Normal If Json Didn`t Get 404 //
+		textArea.classList = 'text-area' ;
+		textArea.placeholder = 'Search City ...' ;
+		
+		// Serving Temp Json Data //
+		mainTemp.innerText = Math.round(jsonData.main.temp - 273.15);
+		degreeF.innerText = Math.round(((jsonData.main.temp - 273.15) * 9 / 5) + 32) + '°F';
+		degreeK.innerText = Math.round(jsonData.main.temp) + '°K';
 
+		// SetUp Country And Place Name To Serve // 
+		placeName.innerText = jsonData.name;
+		countryName.innerText = jsonData.sys.country;
+		weatherCondition.innerText = `${jsonData.wind.speed} W/speed`;
+	}
 }
+
 // Deafult Weather Api Place Location //
 apiData('New York');
 
